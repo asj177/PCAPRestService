054c2ad5c6afa19c2dfa4c4f5badff17f280e1e7
asj177 <arpitjoshi_177@yahoo.com>
2015-10-12 18:26:58 -0700

    JNI

--- src/main/java/demo/JNIWrapper.java
+++ src/main/java/demo/JNIWrapper.java
@@ -7,27 +7,49 @@ public class JNIWrapper {
 	static{
 		
 
-		System.load("/home/arpit/Documents/JNI/latest/libpax_api.so");
+		System.load("/home/arpit/Documents/JNI/newJNI/libJNIDemo.so");
 		
 		//System.loadLibrary("JNIDemo");
 	}
 	
+	//Get Error String
 	public native String pax_get_error_string(int error_code);
-	public native int packet_mining_start(pax_store_mining_query_param_s packet_mining_params);
+	
+	//Get Path in output param and status
+	public native int  packet_mining_start(pax_store_mining_query_param_s packet_mining_params,OutParams filePath);
+	
+	
+	//Get status
 	public native int packet_mining_cancel(String cookies);
+	
+	
+	//Get the fileName and status
 	public native int packet_mining_get_pcap_file_name(String cookies,String fileName);
 	
 	
-//	public static void main(String args[]){
-//		
-//		try{
-//	
-//		JNIWrapper jni=new JNIWrapper();
-//		int result=jni.multiply(7, 8);
-//		System.out.println("Result is "+result);
-//		}catch(Exception e){
-//			e.printStackTrace();
-//		}
-//		
-//	}
+	//Get the Percentage status
+	public static native int pax_packet_mining_get_status(String cookies,OutParams percentage_complete);
+	
+	
+	//Get Mining Stats
+	public static native int pax_packet_mining_get_query_stats(String cookies,PCAPMiningStats miningstat);
+	
+	/*public static void main(String args[]){
+	
+		JNIWrapper jni=new JNIWrapper();
+		int per=70;
+		OutParams out=new OutParams();
+		int result=jni.packet_mining_start(new pax_store_mining_query_param_s(),out);
+		System.out.println(out.getPath()+" reslt os "+result);
+		
+////		try{
+////	
+////		JNIWrapper jni=new JNIWrapper();
+////		int result=jni.multiply(7, 8);
+////		System.out.println("Result is "+result);
+////		}catch(Exception e){
+////			e.printStackTrace();
+////		}
+////		
+	}*/
 }
