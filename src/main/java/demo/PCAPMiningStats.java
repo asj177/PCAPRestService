81b4ad2c60825e87ee1b25ba00c1f523259cd949
asj177 <arpitjoshi_177@yahoo.com>
2016-03-28 01:43:07 -0700

    PCAPMining Stats file

---
+++ src/main/java/demo/PCAPMiningStats.java
@@ -0,0 +1,36 @@
+package demo;
+
+public class PCAPMiningStats {
+	
+	int pkts_matched;
+	int pkts_searched;
+	int pages_searched;
+	int directory_entries_searched;
+	public int getPkts_matched() {
+		return pkts_matched;
+	}
+	public void setPkts_matched(int pkts_matched) {
+		this.pkts_matched = pkts_matched;
+	}
+	public int getPkts_searched() {
+		return pkts_searched;
+	}
+	public void setPkts_searched(int pkts_searched) {
+		this.pkts_searched = pkts_searched;
+	}
+	public int getPages_searched() {
+		return pages_searched;
+	}
+	public void setPages_searched(int pages_searched) {
+		this.pages_searched = pages_searched;
+	}
+	public int getDirectory_entries_searched() {
+		return directory_entries_searched;
+	}
+	public void setDirectory_entries_searched(int directory_entries_searched) {
+		this.directory_entries_searched = directory_entries_searched;
+	}
+	
+	
+
+}
