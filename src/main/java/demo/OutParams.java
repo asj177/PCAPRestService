257600515789f44a3546f9c83ec6f99d9d2fa078
asj177 <arpitjoshi_177@yahoo.com>
2016-03-28 01:41:30 -0700

    New File for out params

---
+++ src/main/java/demo/OutParams.java
@@ -0,0 +1,23 @@
+package demo;
+
+public class OutParams {
+
+	int value;
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	String path;
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
