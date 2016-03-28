4710abab1ceb6bb7683ffe3f77ae94dabb2892ee
asj177 <arpitjoshi_177@yahoo.com>
2016-03-28 01:45:23 -0700

    Error messages added

--- src/main/java/util/ERROR_PCAP_MAPPING.java
+++ src/main/java/util/ERROR_PCAP_MAPPING.java
@@ -2,4 +2,11 @@ package util;
 
 public class ERROR_PCAP_MAPPING {
 
+	public static final String LIBRARY_NOT_LOADED=
+			"Please check if the JNI methods exactly match with the methods specified in the JNIWrapper.c";
+	public static final String ERROR_IN_GETTING_ES_DATA="Internal Error has occured in getting ES data";
+	public static final String ERROR_IN_START_MINING="Internal Error has occured in PCAP Start Mining";
+	public static final String ERROR_IN_PCAP_MINING_STATUS="Internal Error has Occured in PCAP Get Status";
+	public static final String KEY_NOT_PRESENT="Not Authorised,Key not present in request";
+	public static final String ERROR_WHILE_CONNECTING_ES="Error while connecting with Elasticsearch ";
 }
